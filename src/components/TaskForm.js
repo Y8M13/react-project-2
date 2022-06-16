e6527bad6b8bcce80a97881b68a872e24286b854
Yolanda Morris <y.mijoe7145@gmail.com>
2022-06-16 00:35:41 -0400

    Created input fields on TaskForm component

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -2,9 +2,41 @@ import React from 'react';
 
 const TaskForm = () => {
   return (
-    <div>
-      <h2>Add New Task</h2>
-    </div>    
+    <div className="container">
+      <form>
+        <h2>Add New Task</h2>
+        <div>
+          <label htmlFor="date">Date</label>
+          <input type="text" date="date" id="date" />
+        </div>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div>
+          <label htmlFor="category">Category</label>
+          <input type="text" category="category" id="category" />
+        </div>
+        <div>
+          <label htmlhtmlFor="priority">Priority</label>
+          <input type="text" Priority="priority" id="priority" />
+        </div>
+        <div>
+          <label htmlFor="status">Status</label>
+          <input type="text" status="status" id="status" />
+        </div>
+        <div>
+          <label htmlFor="deadline">Deadline</label>
+          <input type="text" deadline="deadline" id="name" />
+        </div>
+        <div>
+          <label htmlFor="comments">Comments</label>
+          <input type="text" comments="comments" id="comments" />
+        </div>
+        <br/>
+        <button type="submit" className="btn">Submit</button>
+      </form>   
+    </div> 
   )
 }
 
--- src/components/TaskLog.js
+++ src/components/TaskLog.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
 
 const TaskLog = ()  => {
+
+    useEffect(() => {
+       fetchTasks();
+    }, []);
+
+   const [tasks, setTasks] = useState([]); 
+
+   const fetchTasks = async () => {
+     const data = await fetch('http://localhost:3000/tasks');
+
+     const tasks = await data.json();
+     console.log(tasks);
+     setTasks(tasks);
+   };
+
   return (
     <div>
-      <h2>Task Log</h2>
+      {tasks.map(task =>(
+        <h1 key={ task.id }>{ task.name }</h1>
+      ))}
     </div>
   )
 }
